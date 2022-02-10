ea2b9edf53dba4ac95f9182ad028978a25ea6b34
ThallesRyan <thallesryan2@gmail.com>
2022-02-10 15:01:07 -0300

    Armazenando valores

--- src/components/layout/input.jsx
+++ src/components/layout/input.jsx
@@ -1,9 +1,17 @@
 export const Input = (props) => {
-    console.log(props)
+    
+
+    const handleChange = event =>{
+        // const {value} = event.target
+        const valor = event.target.value
+        console.log("Mudou ", valor)
+
+        props.setValue(valor)
+    }
     return (
         <div className="form-group">
             <label className="form-label" htmlFor={props.id}>{props.label}</label>
-            <input type={props.type} id={props.id} name={props.name} placeholder={props.placeholder}></input>
+            <input onChange={handleChange} type={props.type} id={props.id} name={props.name} placeholder={props.placeholder}></input>
         </div>
     )
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,29 @@
 import { Input } from "../components/layout/input"
 import { Botao } from "../components/layout/Botao"
+import {useState} from "react"
 
 const Login = () => {
+    let variavel = 'Meu nome é Thalles'
+
+    
+    // const [param1, param2] = useState('Valor Incial')
+    /*
+    UseState retorna
+    Param 1 Valor da minha variável
+    Param 2 Função*/
+    const [email, setEmail] = useState()
+    console.log("Email: "+ email)
+
+    const [password, setPassword] = useState()
+    console.log("Senha: "+ password)
+
     return (
         <div className="container">
             <h1>Login</h1>
             <div id="formLogin">
                 <div className="form">
                     <Input
+                        setValue ={setEmail}
                         label="Email"
                         title="E-mail"
                         type="text"
@@ -16,6 +32,7 @@ const Login = () => {
                         placeholder="Seu email"
                     />
                     <Input
+                        setValue = {setPassword}
                         label="Senha"
                         title="password"
                         type="password"
@@ -23,8 +40,8 @@ const Login = () => {
                         name="senha"
                         placeholder="Sua senha"
                     />
-                   <Botao >Acessar</Botao>
-                   
+                    <Botao >Acessar</Botao>
+
                 </div>
             </div>
         </div>
